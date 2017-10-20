8a82b9fc49fbf298f35433b0d6b04b9e74e06920
Jakob Bussas <jakobbu@gmx.de>
2017-10-20 19:50:20 +0200

    log fix

--- src/log.php
+++ src/log.php
@@ -6,11 +6,11 @@ class log
 	{
 		$ip = getenv('REMOTE_ADDR');
 		$userAgent = getenv('HTTP_USER_AGENT');
-		$referrer = getenv('HTTP_REFERER');
+		$referer = getenv('HTTP_REFERER');
 		$query = getenv('QUERY_STRING');
 
-		$sql = "INSERT INTO access_log (ip, browser, referrer, query) VALUES (:ip, :userAgent, :referrer, :query)";
-		$params = array(":ip" => $ip, ":userAgent" => $userAgent, ":referrer" => $referrer, ":query" => $query);
+		$sql = "INSERT INTO access_log (ip, browser, referer, query) VALUES (:ip, :userAgent, :referer, :query)";
+		$params = array(":ip" => $ip, ":userAgent" => $userAgent, ":referer" => $referer, ":query" => $query);
 		sql::exe($sql, $params);
 	}
 }
